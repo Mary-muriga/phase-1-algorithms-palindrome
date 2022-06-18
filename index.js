115d7d21fd9cd767a3c8ad5a0935f12740da93bb
Marymuriga <marymuriga@gmail.com>
2022-06-18 22:16:10 +0300

    commit tree 769f73c7b3c1d62510411142f098d118b8374336

--- index.js
+++ index.js
@@ -1,27 +1,31 @@
+ function revString(word){
+  const wordArrayVal = word.split("");
+  const revWordArrayVal = wordArrayVal.reverse();
+  const revWord = revWordArrayVal.join("");
+  return revWord;
+
+ }
+
 function isPalindrome(word) {
   // Write your algorithm here
-  const arrVal = word.split('');
-  const revArrVal = arrVal.reverse();
-  const revWord = revArrVal.join('');
-
-
-
-/* 
-  Add your pseudocode here
-*/
-// convert string into an arry value using split()
-// Reverse the array values using reverse()
-// grouping arrays into string using join()
-// use if statement to check if they are equal 
-// print results
+  
+  const revWord = revString(word)
+  //Add your pseudocode here
+ //*/
+ // convert string into an arry value using split()
+ // Reverse the array values using reverse()
+ // grouping arrays into string using join()
+ // use if statement to check if they are equal 
+ // print results
  
-if (word == revWord){
-  console.log('true')
+  if (word === revWord){
+   return true;
+  }
+ else {
+  return false;
+ } 
 }
-else {
-  console.log('false')
-}}
-isPalindrome('abba')
+
 
   //Add written explanation of your solution here
 //*///Take in a string convert it into an array.
@@ -32,18 +36,24 @@ isPalindrome('abba')
  if (require.main === module) {
   // add your own custom tests in here
   console.log('Expecting: true');
-  console.log('=>', isPalindrome("omo"));
+  console.log('=>', isPalindrome("abba"));
+  console.log("");
 
-  console.log('Expecting: false');
-  console.log('=>', isPalindrome("cat"));
+  console.log('Expecting: true');
+  console.log('=>', isPalindrome("racecar"));
+  console.log("");
 
   console.log("Expecting: true");
-  console.log("=>", isPalindrome("racecar"));
-
+  console.log("=>", isPalindrome("a"));
   console.log("");
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("ab"));
+  console.log("");
 }
 
-//module.exports = isPalindrome;
+module.exports = isPalindrome;
